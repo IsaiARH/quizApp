b0a645aef8db092f9f63db3df950b5452da9f799
Isai Hidalgo <arhrexark@gmail.com>
2022-03-30 16:41:29 -0600

    head finish and i add the icons

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import style from "./style.css";
 import anime from "animejs";
+
+import icon from "./imgs/icons/404-error (2).png";
+
+const head = document.querySelector(".header");
+const codeHead = `
+ <h1 class="me-2">QuizDev</h1>
+<div>
+  <img src=${icon} alt="">
+</div>`;
+head.innerHTML = codeHead;
